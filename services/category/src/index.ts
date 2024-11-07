d290ed271007f0ab46591142cc3643736c8b614b
GanaF4ll <ganafall9498@gmail.com>
2024-11-07 12:27:52 +0100

    🔧 🔨 docker compose fix

--- services/category/src/index.ts
+++ services/category/src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/category', CategoryRoute);
 
-const PORT = process.env.PORT || 8004;
-export const server = app.listen(PORT, () => {
-  console.log(`Category-service is running on port ${PORT}`);
+const CATEGORY_PORT = process.env.CATEGORY_PORT || 8004;
+export const server = app.listen(CATEGORY_PORT, () => {
+  console.log(`Category-service is running on port ${CATEGORY_PORT}`);
 });
