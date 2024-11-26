bcb370c47de761dcd9a86720f064b536a3346f5d
GanaF4ll <ganafall9498@gmail.com>
2024-11-26 19:28:46 +0100

    🔨 start dev script

--- services/category/start_dev_script.sh
+++ services/category/start_dev_script.sh
@@ -7,6 +7,8 @@ npx prisma generate
 # Run Prisma migration
 npx prisma migrate deploy
 
+npx prisma db push
+
 npx ts-node-dev --respawn --transpile-only ./src/index.ts
 
 echo "Prisma client launched and migration applied successfully."
